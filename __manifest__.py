0f04331226bf888fb23d82f63b43a27e673c8202
AndreiLe <lebandr2@gmail.com>
2018-10-25 12:53:24 +0300

    [ADD] Init odoo module

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hello World",
+    'summary': """
+        Hello World theme""",
+    'description': """
+        Hello World test theme modul
+    """,
+    'category': 'Theme',
+    'version': '0.1',
+    'depends': ['website'],
+    'data': [
+        'data/hello_data.xml',
+        'views/hello_templates.xml',
+    ]
+}
